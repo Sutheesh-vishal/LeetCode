f02689820f85ad1faf34397f94e3400e25065963
Sutheesh-Vishal <sutheeshvishalnagarajan@gmail.com>
2024-08-09 23:29:24 +0530

    ScoreOfAString

--- String/ScoreOfAString.java
+++ String/ScoreOfAString.java
@@ -8,7 +8,7 @@ public class ScoreOfAString {
         for(int i=0;i<s.length()-1;i++){
 
             //finds ASCII value and adds the difference to the score variable.
-            //abs is to make the number absolute.
+            //abs is to make the number absolute(positive)
             score += Math.abs((int)s.charAt(i)-(int)s.charAt(i+1));
 
         }
