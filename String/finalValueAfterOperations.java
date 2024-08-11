c3f5c27f78b6ed12461a975216ea53f9b9f9c56e
Sutheesh-Vishal <sutheeshvishalnagarajan@gmail.com>
2024-08-11 18:07:43 +0530

    finalValueAfterOperations

---
+++ String/finalValueAfterOperations.java
@@ -0,0 +1,23 @@
+package string;
+
+public class finalValueAfterOperations {
+        public int finalValueAfterOperations(String[] operations) {
+
+            //initilizing variable
+            int x = 0;
+
+             //foreach for identifying the condition
+            for(String operation : operations){
+
+                //incrimenting/decrementing x
+                if(operation.contains("--")) --x;
+                else ++x;
+            }
+            return x;
+        }
+    public static void main(String[] args) {
+        finalValueAfterOperations Coffee = new finalValueAfterOperations();
+        System.out.println(Coffee.finalValueAfterOperations(new String[]{"--X","X++","X++"}));
+    }
+    
+}
