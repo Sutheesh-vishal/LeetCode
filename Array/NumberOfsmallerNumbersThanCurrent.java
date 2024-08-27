7577002083d1c226bfd38c032fce693da5e0cfb8
Sutheesh-Vishal <sutheeshvishalnagarajan@gmail.com>
2024-08-27 23:45:48 +0530

    Solution Added

--- Array/NumberOfsmallerNumbersThanCurrent.java
+++ Array/NumberOfsmallerNumbersThanCurrent.java
@@ -2,7 +2,20 @@ package Array;
 
 public class NumberOfsmallerNumbersThanCurrent {
 
-    
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int[] arr = new int[nums.length];
+        for(int i = 0;i<nums.length;i++){
+            int count = 0;
+            for(int j = 0;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    count++;
+                }
+            }
+            arr[i]=count;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
         NumberOfsmallerNumbersThanCurrent Coffee = new NumberOfsmallerNumbersThanCurrent();
         Coffee.smallerNumbersThanCurrent(new int[]{8,1,2,2,3});
