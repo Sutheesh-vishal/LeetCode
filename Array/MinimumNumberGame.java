3440fd6f6beeac392dd3c90b5d3fca2d62edcd80
Sutheesh-Vishal <sutheeshvishalnagarajan@gmail.com>
2024-08-27 23:52:31 +0530

    Solution Added

---
+++ Array/MinimumNumberGame.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class MinimumNumberGame {
+    public int[] numberGame(int[] nums) {
+
+        int[] arr = new int[nums.length];
+        return arr;
+        
+    }
+    public static void main(String[] args) {
+        MinimumNumberGame Coffee = new MinimumNumberGame();
+        Coffee.numberGame(new int[]{5,4,2,3});
+    }
+}
