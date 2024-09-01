d0a15cda83c800e018ef7c2f59003c65ab52adb7
Sutheesh-Vishal <sutheeshvishalnagarajan@gmail.com>
2024-09-01 15:55:40 +0530

    Solution Added

--- Array/MinimumNumberGame.java
+++ Array/MinimumNumberGame.java
@@ -1,11 +1,20 @@
 package Array;
 
+
+import java.util.Arrays;
+
 public class MinimumNumberGame {
     public int[] numberGame(int[] nums) {
 
-        int[] arr = new int[nums.length];
-        return arr;
-        
+        Arrays.sort(nums);
+        int[] Output = new int[nums.length];
+        for(int i = 0 ; i<nums.length;i+=2){
+            Output[i] = nums[i+1];
+            Output[i+1] = nums[i];
+             
+        }
+        return  Output;
+
     }
     public static void main(String[] args) {
         MinimumNumberGame Coffee = new MinimumNumberGame();
