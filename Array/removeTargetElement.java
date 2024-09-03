2b8c8c5f6b43c4ce75a4f084b1be8d9ba15d5623
Sutheesh-Vishal <sutheeshvishalnagarajan@gmail.com>
2024-09-03 20:08:02 +0530

    Solution Added

---
+++ Array/removeTargetElement.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class removeTargetElement{
+    public static int removeElement(int[] nums, int val){
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]!=val){
+                nums[index]=nums[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+    public static void main(String[] args) {
+        removeTargetElement Coffee = new removeTargetElement();
+        System.out.println(Coffee.removeElement(new int[]{0,1,2,2,3,0,4,2},2));
+    }
+}
