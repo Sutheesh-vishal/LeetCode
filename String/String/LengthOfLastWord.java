6514b34c7be9a7264bcc9ad18c4e1bc605c51f6e
Sutheesh-Vishal <sutheeshvishalnagarajan@gmail.com>
2024-09-14 21:43:42 +0530

    Solution Added

---
+++ String/String/LengthOfLastWord.java
@@ -0,0 +1,23 @@
+package String;
+
+
+public class LengthOfLastWord{
+    public int lengthOfLastWord(String s) {
+        
+        String trimmed = s.trim();
+        int count = 0;
+        for(int i = trimmed.length()-1;i>=0;i--){
+            if(trimmed.charAt(i)!=' '){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        LengthOfLastWord Coffee = new LengthOfLastWord();
+        System.out.println(Coffee.lengthOfLastWord("Hello World"));
+    }
+}
