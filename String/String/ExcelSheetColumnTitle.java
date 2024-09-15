29433a01c6c697d908b80f5a4554133b5f8939bf
Sutheesh-Vishal <sutheeshvishalnagarajan@gmail.com>
2024-09-15 21:39:37 +0530

    Solution Added

---
+++ String/String/ExcelSheetColumnTitle.java
@@ -0,0 +1,17 @@
+package String;
+
+public class ExcelSheetColumnTitle {
+        public String convertToTitle(int columnNumber) {
+            StringBuilder s = new StringBuilder();
+            while(columnNumber > 0){
+                int i =  (columnNumber-1)%26;
+                s.append((char)(i+'A'));
+                columnNumber=(columnNumber-1)/26;  
+            }
+            return s.reverse().toString();
+        }
+    public static void main(String[] args) {
+        ExcelSheetColumnTitle Coffee = new ExcelSheetColumnTitle();
+        System.out.println(Coffee.convertToTitle(28));
+    }
+}
